c7eec31d6cb60c69017a0d2601bb589703b643a8
Franekkk <peterek94@gmail.com>
2015-07-28 22:40:31 +0200

    First try.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,10 +9,34 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/app/example", name="homepage")
+     * @Route("/", name="homepage")
      */
     public function indexAction()
     {
-        return $this->render('default/index.html.twig');
+        return $this->render('pages/index.html.twig');
+    }
+
+    /**
+     * @Route("/products", name="products")
+     */
+    public function productsAction()
+    {
+        return $this->render('pages/products.html.twig');
+    }
+
+    /**
+     * @Route("/about", name="about")
+     */
+    public function aboutAction()
+    {
+        return $this->render('pages/about.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contactAction()
+    {
+        return $this->render('pages/contact.html.twig');
     }
 }
